e8fe66a740afe654fd381de44fb558a00317f583
Ayush Raj <yoayushraj99@gmail.cmom>
2021-06-26 00:22:41 +0530

    initiated

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from './components/header/header.component';
+import HomePage from './Pages/homepage/homepage';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        {/* <Route path="/shop" component={ScheduleMail} />
+        <Route exact path="/checkout" component={History} /> */}
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
