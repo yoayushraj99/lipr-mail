491aa2470c41fdc7f8390a5224ba95a15502d68d
anwersayeed <deeyasrewna@gmail.com>
2021-06-26 11:32:00 +0530

    Google OAuth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,18 +4,24 @@ import Sidebar from './components/sidebar/sidebar';
 import HomePage from './Pages/homepage/homepage';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Compose from './components/compose/Compose';
+import NewCompose from './components/compose/NewCompose'
 
 function App() {
   return (
     <div>
       
       <Route exact path="/" component={Sidebar} />
-      <Route exact path="/c" component={Compose} />
+      
       {/* <Switch>/ */}
       {/* <Route exact path="/" component={HomePage} /> */}
       {/* <Route path="/shop" component={ScheduleMail} />
         <Route exact path="/checkout" component={History} /> */}
       {/* </Switch> */}
+
+
+      {/* Testing */}
+      <Route exact path="/c" component={Compose} />
+      <Route exact path="/nc" component={NewCompose} />
     </div>
   );
 }
