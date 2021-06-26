2e0d1af2cd7835198a417f6d25a83093b88fe3e4
Ayush Raj <yoayushraj99@gmail.cmom>
2021-06-26 23:58:04 +0530

    prettified

--- backend/controller/sendMail.js
+++ backend/controller/sendMail.js
@@ -9,7 +9,7 @@ var smtpTransport = nodemailer.createTransport({
 });
 
 module.exports.sendResetEmail = async (email, token) => {
-   // change first part to your domain
+  // change first part to your domain
   var url = "http://localhost:8000/user/reset-password?token=" + token;
 
   await smtpTransport.sendMail({
@@ -33,3 +33,29 @@ module.exports.sendVerifyEmail = async (email, token) => {
     html: `<h3> Click on this link to verify your email : ${url} </h3>`,
   });
 };
+
+module.exports.sendComposedMail = async ({
+  senderMail,
+  primaryRecieverMail,
+  ...otherProps
+}) => {
+  const mailOptions = {
+    from: { senderMail },
+    to: { primaryRecieverMail },
+    cc: { secondaryRecieverMail },
+    subject: { subject },
+    text: { body },
+  };
+
+  await smtpTransport.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(`Email send: ${info.response}`);
+    }
+  });
+};
+
+cron.schedule("* * * * * *", () => {
+  console.log("email sent");
+});
